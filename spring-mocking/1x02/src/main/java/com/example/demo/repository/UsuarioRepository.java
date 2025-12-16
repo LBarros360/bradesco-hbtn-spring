9f1a2cae4f6a5a82aa5d1d89730b7236c64a5b27
LBarros360 <lbarros360@gmail.com>
2025-12-16 18:11:58 -0300

    Create UsuarioRepository.java

---
+++ spring-mocking/1x02/src/main/java/com/example/demo/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Usuario;
+
+import java.util.Optional;
+
+public interface UsuarioRepository {
+
+    Optional<Usuario> findById(Long id);
+
+    Usuario save(Usuario usuario);
+
+}
